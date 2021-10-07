8d23169df81518d72d86e5d5ace13ef5565a2e94
lanly_dev <lanly-dev@users.noreply.github.com>
2021-10-06 23:29:04 -0700

    Minor changes of build.ts

--- build.ts
+++ build.ts
@@ -1,31 +1,27 @@
-#!/usr/bin/env ts-node
-import { build }  from 'vite'
+import { build } from 'vite'
 import { dirname } from 'path'
 
-const vConfigs = [
-  'vite.config.ts',
-  'vite.electron.config.ts'
-]
+const viteConfigs = ['vite.config.ts', 'vite.electron.config.ts']
 
-const buildByConfig = (configFile: string) => build({ configFile })
 ;(async () => {
   try {
     const totalTimeLabel = 'Total bundling time'
     console.time(totalTimeLabel)
 
-    for (const p of vConfigs) {
-      const consoleGroupName = `${dirname(p)}/`
+    for (const configFile of viteConfigs) {
+      const consoleGroupName = `${dirname(configFile)}/`
       console.group(consoleGroupName)
 
       const timeLabel = 'Bundling time'
       console.time(timeLabel)
 
-      await buildByConfig(p)
+      await build({ configFile })
 
       console.timeEnd(timeLabel)
       console.groupEnd()
       console.log('\n')
     }
+
     console.timeEnd(totalTimeLabel)
     console.log('\n')
   } catch (e) {
